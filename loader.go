f2e5b79b83eb8e277db88d4a4c107daf127ffa6c
Alex Mushket <alex.mushket@wiseweb.co>
2017-03-31 19:36:34 +0300

    load profiles&relations

--- loader.go
+++ loader.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/garyburd/redigo/redis"
-	"io"
 	"os"
 	"strings"
 	"time"
@@ -21,44 +20,115 @@ func timeTrack(start time.Time, name string) {
 	fmt.Printf("%s took %s\n", name, elapsed)
 }
 
-func main() {
-	r, err := redis.Dial("tcp", ":6389")
-	defer r.Close()
+func importProfiles(rds *redis.Conn, filename string) {
 
-	str, err := r.Do("GET", "ololo")
+	defer timeTrack(time.Now(), "PROFILES IMPORT")
+
+	//get columns
+	var cols []string
+	col_file, err := os.Open("data/columns.txt")
 	PanicOnError(err)
-	fmt.Printf("Value: %s", str)
+	defer col_file.Close()
 
-	defer timeTrack(time.Now(), "iMPORT")
-
-	f, err := os.Open("data/soc-pokec-profiles.txt")
-
-	rdr := bufio.NewReader(f)
-
-	i := 0
-	rec, isPref, err := rdr.ReadLine()
-
-	if isPref {
-		// 	s, isPref, err := rdr.ReadLine()
-
-		// 	rec = append(rec, s)
+	scanner := bufio.NewScanner(col_file)
+	for scanner.Scan() {
+		cols = append(cols, scanner.Text())
 	}
 
-	for err != io.EOF {
+	fmt.Println("COLUMNS ARE:", cols)
+
+	//get data
+	data_file, err := os.Open(filename)
+	defer data_file.Close()
+
+	i := 0
+
+	scanner = bufio.NewScanner(data_file)
+
+	const maxCapacity = 256 * 1024
+	buf := make([]byte, maxCapacity)
+	scanner.Buffer(buf, maxCapacity)
+
+	for scanner.Scan() {
+		rec := scanner.Text()
+
+		arr := strings.Split(string(rec), "\t")
+		key := "person:" + arr[0]
+
+		var data []interface{}
+
+		data = append(data, key)
+		data = append(data, zip_arrays(cols, arr)...)
+
+		// msg, err :=
+		(*rds).Do("HMSET", data...)
+		// _, err := r.Do("SETEX", arr[0], 60, arr)
+		// break
 		i++
 		if i%100000 == 0 {
 			fmt.Println("Processed so far :", i)
 		}
 
-		// if rec == nil {}
-		arr := strings.Split(string(rec), "\t")
-		_, err := r.Do("SETEX", arr[0], 3600, arr)
-
-		// fmt.Printf("%s\n", strings.Split(string(rec), "\t"))
-
-		rec, isPref, err = rdr.ReadLine()
-		PanicOnError(err)
 	}
-	fmt.Println("Processed all :", i)
 
+	fmt.Println("Processed all :", i)
+}
+
+func importRelations(rds *redis.Conn, filename string) {
+
+	i := 0
+	graph := "pokec"
+
+	// Processed all : 30622564
+	// RELATIONS IMPORT took 1h24m45.771996371s
+	defer timeTrack(time.Now(), "RELATIONS IMPORT")
+
+	//get data
+	data_file, err := os.Open(filename)
+	PanicOnError(err)
+	defer data_file.Close()
+
+	scanner := bufio.NewScanner(data_file)
+
+	for scanner.Scan() {
+		rec := scanner.Text()
+		arr := strings.Split(string(rec), "\t")
+
+		person := "person:" + arr[0]
+		friend := "person:" + arr[1]
+
+		_, err := (*rds).Do("GRAPH.ADDEDGE", graph, person, "friend", friend)
+		// fmt.Println(msg, err)
+		if err != nil {
+		}
+		break
+		i++
+		if i%100000 == 0 {
+			fmt.Println("Processed so far :", i)
+			break
+		}
+
+	}
+
+	fmt.Println("Processed all :", i)
+}
+
+func main() {
+
+	rds, err := redis.Dial("tcp", ":6379")
+	PanicOnError(err)
+	defer rds.Close()
+
+	importProfiles(&rds, "data/soc-pokec-profiles.txt")
+	importRelations(&rds, "data/soc-pokec-relationships.txt")
+
+}
+
+func zip_arrays(a, b []string) []interface{} {
+	res := make([]interface{}, len(a)*2, len(a)*2)
+	for i, x := range a {
+		res[i*2] = x
+		res[i*2+1] = b[i]
+	}
+	return res
 }
